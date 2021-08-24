2b0efd8f1f168a8fcca47e2829f7c206b04838ba
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-08-24 14:24:58 +0200

    update ex_6

--- Code-Along/Excercise_6.py
+++ Code-Along/Excercise_6.py
@@ -9,4 +9,4 @@ p_2 = [3,1, 0]
 # calcutate distance with formula
 dist = math.sqrt(math.pow((p_2[1]-p_1[1]),2) + math.pow((p_2[0]-p_1[0]),2) + math.pow((p_2[2]-p_1[2]),2))
 
-print(f"distance is {dist}")
+print(f"The distance is {dist:.2f}")
