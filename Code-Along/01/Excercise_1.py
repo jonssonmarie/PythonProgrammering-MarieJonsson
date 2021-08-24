54f9adcea02ea996c0a8936cdb20f486752d8041
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-08-24 16:29:17 +0200

    new files and folders

---
+++ Code-Along/01/Excercise_1.py
@@ -0,0 +1,13 @@
+# Ask the user to input a number and check if this number is 
+# positive, negative or zero and print it out.
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("Number is a positive") 
+if number == 0:
+    print("Number is zero")
+if number < 0:
+    print("Number is negative")
+
+
