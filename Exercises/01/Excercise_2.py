936dd1160ce7b858b6389b3e8b6a366448f4899e
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-08-27 09:05:40 +0200

    update ex_2

--- Exercises/01/Excercise_2.py
+++ Exercises/01/Excercise_2.py
@@ -2,15 +2,16 @@
 # Ask the user to input two numbers and check which one 
 # is the smallest and print it out.
 
-numbers = input("Enter two integers separated with ,  : ")
+numbers = input("Enter two integers separated with , : ")
 num_list = numbers.split(",")
 
 num_1 = int(num_list[0])
 num_2 = int(num_list[1])
 
+# check if equal or smallest number
 if num_1 == num_2:
     print(f"{num_1} and {num_2} is equal")
-if num_1 < num_2:
+elif num_1 < num_2:
     print(f"{num_1} is the smallest number")
 else:
     print(f"{num_2} is the smallest number")
