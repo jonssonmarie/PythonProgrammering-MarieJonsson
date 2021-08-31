b58b9a12a75a0a37bdf8308e34be0c8c9282037f
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-08-31 12:00:52 +0200

    upd

--- Exercises/02/Excersise_03.py
+++ Exercises/02/Excersise_03.py
@@ -4,12 +4,12 @@
 #    Can you optimize to get as few guesses as possible?
 
 import random as rnd
-
+"""
 n = 0
 
 while n < 10:
     rand_num = rnd.randint(1, 100)
-    guess = int(input("Guess the number the computer gives, 1 - 100 : "))
+    guess = int(input("Give a number for the computer to guess, 1 - 100 : "))
     if rand_num == guess:
         print(" Correct")
     elif rand_num < guess:
@@ -17,6 +17,25 @@ while n < 10:
     elif rand_num > guess:
         print("Guess a higher number")
     n += 1
+"""
+# b) - ej fintrimmad
 
-   # b) ej klar
+low = 1
+high = 100
+i = 0
+
+user_num = int(input("Give a number for the computer to guess, 1 - 100: "))
+comp_guess = rnd.randint(low, high)
+
+while comp_guess != user_num :
+    if comp_guess > user_num:
+        high = comp_guess - 1
+        comp_guess = rnd.randint(low, high)
+    else:
+        low = comp_guess + 1
+        comp_guess = rnd.randint(low, high)
+    i +=1
+    print("No of loops:", i)
+    print("The computer takes a guess...", user_num, comp_guess)
+    print(f"Correct guess: {comp_guess} = {user_num}")
 
