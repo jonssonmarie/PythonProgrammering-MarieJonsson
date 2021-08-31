6b348f0846fbda5dd1420a6cfaa4faac957337d3
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-08-31 15:32:43 +0200

    upd

---
+++ Exercises/03/Excersise_06.py
@@ -0,0 +1,14 @@
+"""
+Rice on chessboard
+In first square of the chessboard there is one grain of rice, 
+in the second square there is two grains, 
+in the third square there is four grains and so on. 
+How many grains are there in the whole chessboard when all squares 
+are filled using this pattern?
+"""
+
+
+no_rice = 1
+for i in range(0,64):
+    no_rice += (2**i)
+print(no_rice)
