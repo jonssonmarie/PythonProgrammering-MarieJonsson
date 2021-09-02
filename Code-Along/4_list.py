773fb9dd0d8922ee56a7cd23bd78299454f51456
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-02 09:25:16 +0200

    upd

---
+++ Code-Along/4_list.py
@@ -0,0 +1,20 @@
+"""
+Schema
+9.00 - 11.45
+  - stand up
+         - studieteknik jag använder
+  - listor
+  - kl 11 Sofia kommer och tar startpuls + klassråd
+
+
+
+13.15 -16.00
+  - stuga
+
+ word_len = [len(word) for word in words if word != "the"]
+
+[gör detta för x i denna om x != något]
+"""
+
+
+
