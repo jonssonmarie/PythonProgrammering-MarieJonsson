9102384522db3a0731d20c8854658a9c2b04c37a
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-06 13:45:21 +0200

    upd

---
+++ Exercises/05/Excersise_03.py
@@ -0,0 +1,16 @@
+"""
+Palindrome
+A palindrome is a sequence of characters that is the same, 
+when read forward as backwards (ignoring spaces). 
+For example:
+"Anna" is a palindrome
+"Ni talar bra latin"
+bjkjb
+Let the user input a sequence of characters and check if it is a palindrome.
+"""
+
+
+
+
+
+
