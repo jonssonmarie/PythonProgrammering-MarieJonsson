7241d1dc4433c01f6899a838b16d6645e33f6b79
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-06 14:15:26 +0200

    new

--- Exercises/05/Excersise_03.py
+++ Exercises/05/Excersise_03.py
@@ -9,8 +9,25 @@ bjkjb
 Let the user input a sequence of characters and check if it is a palindrome.
 """
 
+sentence = input("Enter a sentence to check if palindrome: ")
+sentence.replace(" ", "")  # tar bort whitespace mellan orden
 
+lst = [] 
 
+# loop for each letter in sentence and append to lst
+for i in sentence:
+    if i.isupper():             # change to lowercase
+        lst.append(i.lower())
+    if i.islower():             # remain lower case
+        lst.append(i)
+    if not i.isalpha():         # skip if not alfanumeric
+        continue
 
-
+#print(lst)   
+#  
+# check if letters are the same counting from first pos and last pos, stepping each way
+if lst == lst[::-1]:
+    print("This is a palindrome")
+else:
+    print("This is not a palindrome")
 
