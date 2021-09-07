a1d0c1926c49d079b5c6db60851e571b5e39909c
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-07 12:00:19 +0200

    new excesise 03

---
+++ Exercises/06/Excersise_03.py
@@ -0,0 +1,11 @@
+"""
+Mathematical functions
+Make the following functions with def or lambda and plot their
+graphs in the same figure window, with x ∈[−10,10]:
+
+  a) f(x)=x2−3
+  b) g(x)=4x−7
+
+What could the relation between  f(x)  and  g(x)  be?
+"""
+
