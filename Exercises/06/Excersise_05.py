01f014bd87e1d92959785d5a1f278e3439c946f9
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-07 14:12:43 +0200

    05 new

---
+++ Exercises/06/Excersise_05.py
@@ -0,0 +1,15 @@
+"""
+Change
+Create a function that takes a value as input parameter and print out 
+the banknotes and coins in Swedish currency representing this value. 
+For example 5289 would give the following printout:
+
+5st 1000-lapp
+1st 200-lapp
+1st 50-lapp
+1st 20-lapp
+1st 10-krona
+1st 5-krona
+2st 2-krona
+Now let the user input a value, and use the function to calculate the change.
+"""
