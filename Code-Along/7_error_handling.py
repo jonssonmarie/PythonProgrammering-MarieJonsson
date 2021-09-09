8e339ba17b3a388e522acc319d89be467756b30b
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-09 09:42:18 +0200

    update

--- Code-Along/7_error_handling.py
+++ Code-Along/7_error_handling.py
@@ -9,3 +9,48 @@ Dagens schema:
     - jobba själv
     - repetera gammalt
 """
+
+# Error types
+# avmarkera för test          prin("hej")
+
+# a practice of reading error messages
+"""
+ovanstående ger detta svar:
+NameError med vilken rad mm
+"""
+"""
+- syntax error - Python parser don't aunderstand the code
+- logical errors - hard dot find - gives unpredictable results
+- exceptions - Python parser know what to do, but can't do it
+"""
+
+# logical error
+import math
+print(math.pi)
+
+area_circle = lambda radius: 2*math.pi*radius
+
+print(area_circle(10))
+
+# hard to find logical error Area for a circle is 2pi*r^2. 
+# Omkrets = 2pi*r
+
+# exception errors
+
+numbers = list(range(10)) # index 10 doesn't exist in this list which ends at 9
+print(numbers)  # -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#print(numbers[10])
+
+print("hej")
+namn = input("Vad heter du?: ")
+#   print = namn  # farligt print har blivit ett variabelnamn 
+#   och skriver över functionen print
+
+# ett annat vanligt fel är import tex plotlib och samtidigt har en fil som heter som modulen
+# plotlib.py
+
+
+
+
+
+
