12e822eb3eceb26324c2d565e3367731f35201d7
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-09 09:20:01 +0200

    new

---
+++ Code-Along/7_error_handling.py
@@ -0,0 +1,11 @@
+"""
+Dagens schema:
+9-11.45 Föreläsning
+    - ledningsgruppmöte 21 sept
+        - visualiseringskurs byts ut mot linjär algebra - Kokchun
+    - felhantering
+    - 
+13.15- 16.00 Stuga
+    - jobba själv
+    - repetera gammalt
+"""
