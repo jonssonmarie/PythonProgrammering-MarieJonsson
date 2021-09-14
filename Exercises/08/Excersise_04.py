e18959ada2aba23107250f1912f89e582c67ec9a
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-14 13:10:20 +0200

    new 04

---
+++ Exercises/08/Excersise_04.py
@@ -0,0 +1,7 @@
+"""
+Dice roll experiment
+Simulate 10, 100, 1000, 10000, 100000 dice rolls and count the freqencies 
+and probabilities for each number in each simulation. 
+Create a new text file using Python with the name "simulation.txt" 
+and write the results to that text file.
+"""
