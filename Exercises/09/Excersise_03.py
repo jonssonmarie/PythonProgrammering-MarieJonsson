d1ac39f1618f597b1c04b645e3d78839ec31cd90
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-14 16:59:27 +0200

    update

--- Exercises/09/Excersise_03.py
+++ Exercises/09/Excersise_03.py
@@ -13,5 +13,20 @@ Elektrisk, 25
 
 path = "../files/pokemon_list.txt"
 
-fi = path.read()
-print(fi)
+pokedex = {} # "pokemon": "type, index"
+
+with open(path, "r", encoding ="utf-8") as f1:
+    datan = f1.readlines()
+    #print(datan)
+    
+    for data in datan:
+        d = data.strip(" \n")
+        d_split = d.split(" ")
+        val = d_split[2], d_split[0]
+        val_set = ", ".join(val)
+        pokedex[d_split[1]] = val_set
+        
+            
+print(pokedex["Dragonair"])
+print(pokedex["Dragonite"])
+print(pokedex["Pikachu"])
