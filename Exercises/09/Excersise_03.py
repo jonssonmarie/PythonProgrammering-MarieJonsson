1db2236882c2b67aaf8fb3e082a519407e53bb9f
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-14 16:02:52 +0200

    add pokemon_list and 03

---
+++ Exercises/09/Excersise_03.py
@@ -0,0 +1,12 @@
+"""
+Pokemon list
+Read in the file pokemon_list.txt in Python. 
+Create a variable with name pokedex with the 
+key:value "pokemon":"type, index". 
+For example when searching for the keys "Gengar" and "Pikachu":
+print(pokedex["Gengar"])
+print(pokedex["Pikachu"])
+ 
+Spöke/Gift, 94
+Elektrisk, 25
+"""
