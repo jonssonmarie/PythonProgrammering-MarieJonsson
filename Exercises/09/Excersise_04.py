debe4ef9345f292813e8db3f83db10a0e9bf1cfb
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-14 17:03:26 +0200

    new, 04

---
+++ Exercises/09/Excersise_04.py
@@ -0,0 +1,13 @@
+"""
+Morse code
+Read in the file morse.txt, save it in a dictionary and create a 
+function that lets the user input a message to get it translated to morse code. 
+For example
+
+print(morse["SOS"])
+print(morse["POKEMON"])
+ 
+...---...
+.------.-.------.
+
+"""
