3072901c85a39da2b860d1ee9d77ad2d634ed074
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-14 11:56:08 +0200

    upd 03

--- Exercises/08/Excersise_03.py
+++ Exercises/08/Excersise_03.py
@@ -9,50 +9,47 @@ import matplotlib.pyplot as plt
 
 list_2a = []
 list_2c = []
-data_alpha = []
 
-path_2a = "Files/NPvt19Ma2A.txt" # "C:/Users/trull/Documents/GitHub/PythonProgrammering-MarieJonsson/Code-Along/Files/NPvt19Ma2A.txt"
-path_2c = "Files/NPvt19Ma2C.txt" #"C:/Users/trull/Documents/GitHub/PythonProgrammering-MarieJonsson/Code-Along/Files/NPvt19Ma2C.txt"
+path_2a = "C:/Users/trull/Documents/GitHub/PythonProgrammering-MarieJonsson/Code-Along/Files/NPvt19Ma2A.txt"
+path_2c = "C:/Users/trull/Documents/GitHub/PythonProgrammering-MarieJonsson/Code-Along/Files/NPvt19Ma2C.txt"
+
 with open(path_2a, "r") as f2a, open(path_2c, "r") as  f2c:
     data_2a = f2a.readlines()
     data_2c = f2c.readlines()
-    #print(data_2c)
-    """
-    läs in
-    rensa data \n och %
-    lägg i listor en med bokstäver och en med siffror
-    """
+    
     def clean_text(datan, a_list):
         for data in datan:
             d = (data.strip(" \n").strip("%"))
             d_split = d.split(" ")
-            
             a_list.append(d_split)
-            data_alpha.append(d_split[0])
-            print(d_split)
+            
 
     clean_text(data_2a, list_2a)
     clean_text(data_2c, list_2c)
    
- 
+   
     grade_2a = []
     percentage_2a = []
     grade_2c = []
     percentage_2c = []
 
-    #fig1, d = plt.subplot()
-    for x in list_2a:
-        grade_2a.append(x[0])
-        percentage_2a.append(x[1])
+
+    def get_to_list(list_x, grade_x, perc_x):
+        for x in list_x:
+            grade_x.append(x[0])
+            perc_x.append(x[1])
     
-    for x in list_2c:
-        grade_2c.append(x[0])
-        percentage_2c.append(x[1])
-    
-    plt.plot(grade_2a, percentage_2a)
-    plt.pie(percentage_2a)
+    get_to_list(list_2a, grade_2a, percentage_2a)
+    get_to_list(list_2c, grade_2c, percentage_2c)
+
+
+    # subplot
+    plt.figure()
+    plt.subplot(1,2,1)
     plt.title("NPvt19Ma2A")
-    plt.show()
+    plt.pie(percentage_2a,labels=grade_2a)
 
-
-
+    plt.subplot(1,2,2)
+    plt.title("NPvt19Ma2C")
+    plt.pie(percentage_2c,labels=grade_2c)
+ 
