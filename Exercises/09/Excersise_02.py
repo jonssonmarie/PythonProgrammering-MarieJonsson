0152c6d97f8c324e108e077c54e571e23114b8f2
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-14 16:00:07 +0200

    new 02

--- Exercises/09/Excersise_02.py
+++ Exercises/09/Excersise_02.py
@@ -1,8 +1,23 @@
 """
-Dice simulation (*)
+Dice simulation 
 Simulate 1000000 dice rolls and save the number of 
 ones, twos, ..., sixes in a dictionary. 
 Then print them out in the terminal.
-
-
 """
+
+import random as rnd
+
+dices = {}
+
+for i in range(0,1000000):
+    dice = rnd.randint(1,6)
+    dices[i] = dice
+
+
+print("No 1:",list(dices.values()).count(1))
+print("No 2:",list(dices.values()).count(2))
+print("No 3:",list(dices.values()).count(3))
+print("No 4:",list(dices.values()).count(4))
+print("No 5:",list(dices.values()).count(5))
+print("No 6:",list(dices.values()).count(6))
+
