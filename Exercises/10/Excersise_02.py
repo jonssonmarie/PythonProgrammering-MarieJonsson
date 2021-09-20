110b92225bf31504cd680740c52f5c1a3f6f1bcf
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-20 16:00:25 +0200

    Excersise_02 new

---
+++ Exercises/10/Excersise_02.py
@@ -0,0 +1,22 @@
+"""
+Person
+Create a class named Person, with parameterized constructor with the 
+following parameters:
+
+name
+age
+email
+Turn name, age, email into properties with following validations in 
+their setters:
+
+name - must be string
+age - must be number between 0 and 125
+email - must include an @ sign
+It should also have __repr__ method to represent the Person class 
+in a neat way.
+
+Also create a method say_hello() that prints
+
+Hi, my name is ..., I am ... years old, my email address is ...  
+"""
+
