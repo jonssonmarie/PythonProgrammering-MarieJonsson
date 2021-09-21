ee63fc2ba7f0ff36e33b2f43bd6c87ac1ec5f04f
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-21 15:06:43 +0200

    upd 01

--- Exercises/11/Excersise_01.py
+++ Exercises/11/Excersise_01.py
@@ -19,3 +19,44 @@ TV series with title Pokemon, genre Cartoon, rating 4.5 and episodes 550
 Movie with title Titanic, genre Romance, rating 4.7, duration 194 minutes
 Video with title The Code, genre Math and rating 4
 """
+
+
+class Video:
+    def __init__(self, title: str, gender: str, rating: float) -> None:
+        self.title = title
+        self.gender = gender
+        self.rating = rating
+
+
+class TV_serie:
+    def __init__(self, title: str, genre: str, rating: float, num_episodes: int) -> None:
+        self.title = title
+        self.genre = genre
+        self.rating = rating
+        self.num_episodes = num_episodes
+
+
+class Movie:
+    def __init__(self, title: str, genre: str, rating: float, duration: float) -> None:
+        self.title = title
+        self.genre = genre
+        self.rating = rating
+        self.duration = duration
+
+
+class Documentary:
+    def __init__(self, title: str, genre: str, rating: float) -> None:
+        self.title = title
+        self.genre = genre
+        self.rating = rating
+
+
+"""
+Use this code for test the program
+pokemon = TV_serie("Pokemon", "Cartoon", 4.5, 550)
+titanic = Movie("Titanic", "Romance", 4.7, 194)
+code = Documentary("The Code", "Math", 4)
+
+for video in tuple((pokemon, titanic, code)):
+    print(video.info())
+"""
