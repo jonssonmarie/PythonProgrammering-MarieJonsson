20dd69d1e67fa5c8613d570f1ceed99cf9166f71
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-21 14:36:36 +0200

    saved task notes for 01

---
+++ Exercises/11/Excersise_01.py
@@ -0,0 +1,21 @@
+"""
+Video
+Create classes following this UML: see picture in github
+
+Note that the method info() should be different in the different classes where it should be implemented.
+
+Use the following code to test your program:
+pokemon = TV_serie("Pokemon", "Cartoon", 4.5, 550)
+titanic = Movie("Titanic", "Romance", 4.7, 194)
+code = Documentary("The Code", "Math", 4)
+
+
+for video in tuple((pokemon, titanic, code)):
+    print(video.info())
+
+
+An example output could be:
+TV series with title Pokemon, genre Cartoon, rating 4.5 and episodes 550
+Movie with title Titanic, genre Romance, rating 4.7, duration 194 minutes
+Video with title The Code, genre Math and rating 4
+"""
