85252c0d338b8dd00040f97a4942e8b7e0f7f317
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-21 14:39:43 +0200

    save task notes for 04

---
+++ Exercises/10/Excersise_04.py
@@ -0,0 +1,20 @@
+"""
+Simple Travian
+Travian is a strategy game where you collect resources (lumber, clay, iron, crop) 
+to build up a city and spawn troops to pillage and attack other villages. 
+Now we will use OOP to represent a very simple village. 
+It shall have at least these features:
+
+4 fields
+1 crop
+1 clay
+1 lumber
+1 iron
+
+Each field produces 4 units of that resource per hour. Just need to represent it, no need to count the time.
+Be able to add and subtract resources -> overload the plus and minus operators
+There shall be a max capacity of 800 for each resource in the warehouse stocks
+__repr__ method to represent the production rate and current stock.
+Use composition and/or inheritance in a strategic way.
+Feel free to go beyond and implement more features.
+"""
