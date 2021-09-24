425e29e5f475b01944ba1c179ef7836589ee5ad7
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-09-24 12:21:11 +0200

    update exc 03

--- Exercises/10/Excersise_03.py
+++ Exercises/10/Excersise_03.py
@@ -1,33 +1,30 @@
 """
-Student and Teacher 
+Student and Teacher
 Create two classes named Student and Teacher that inherits from Person.
 
 The Student class shall have:
-study() method that prints out
+ - study() method that prints out: "study...study...study...more study"
 
-study...study...study...more study
-override say_hello() with the following message:
+ override say_hello() with the following message: Yo, I am a student, my name is ..., I am ... years old, my email address is ...
 
-Yo, I am a student, my name is ..., I am ... years old, my email address is ...  
 The Teacher class shall have:
+ - teach() method that prints out: teach...teach...teach...more teaching
 
-teach() method that prints out
-teach...teach...teach...more teaching
-Instantiate a Teacher object and a Student object. Call
+Instantiate a Teacher object and a Student object.
 
+Call
 teach() and say_hello() methods from your Teacher object.
 study() and say_hello() methods from your Student object.
 """
-#  INTE KLAR 
 
 import re
 
+
 class Person:
-    def __init__(self, name, age, email, workplace):
+    def __init__(self, name: str, age: int, email: str):
         self.name = name
         self.age = age
         self.email = email
-        self.workplace = workplace
 
     @property
     def name(self) -> str:
@@ -38,7 +35,6 @@ class Person:
         if not isinstance(value, str):
             raise Exception(f"Please use string and not{type(value)}")
         self._name = value
-
 
     @property
     def age(self) -> float:
@@ -62,31 +58,50 @@ class Person:
         else:
             raise TypeError(f"{type(value)} is not valid as email, xxxx@yyyy.zzz is the correct format\n")
 
+    def say_hello(self) -> str:
+        print(f"Mu name is {self.name}, and I am {self.age} years old, you can reach me at: {self.email}")
+
 
 class Student(Person):
-    def study(self):
-        return f"study...study...study...more study\n"
+    def __init__(self, name: str, age: int, email: str, program: str):
+        super().__init__(name, age, email)  # self is automatically sent in through super()
+        self.program = program
 
-    def say_hello(self,):
-        return f"Yo, I am a student, my name is {self.name}, I am {self.age} years old, my email address is {self.email}\n"
+    def study(self) -> str:
+        print(f"study...study...study...more study")
 
-
-class Teacher(Student):
-    def teach(self):
-        return f"teach...teach...teach...more teaching"
-
-    def say_hello(self):
-        return f"Yo, I am a teacher my name is {self.name}, I am {self.age} years old, my email address is {self.email} and I work at {self.workplace}\n"
-        #return Student.say_hello(self)
+    def say_hello(self) -> str:
+        print(f"Yo, I am a student, my name is {self.name}, I am {self.age} years old, my email address is: {self.email}\n")
 
 
-student1 = Student("Maggan", 23, "maggan@student.se", "")
-print(student1.study())
-print("Student class: ",student1.say_hello())
+class Teacher(Person):
+    def __init__(self, name: str, age: int, email: str, workplace: str, program: str) -> None:
+        super().__init__(name, age, email)   # ta in de instanse variabler (tex self.age) du vill över till class Person här. self behövs inte för den kommer automatiskt
+        self.workplace = workplace
+        self.program = program
 
-teacher1 = Teacher("Gudrun", 51, "gudrun@teacher.com", "ITHS")
-print("Teacher class:",teacher1.say_hello())
+    def teach(self) -> str:
+        print(f"teach...teach...teach...more teaching")
+
+    def say_hello(self) -> str:
+        # override say_hello in Person class since say_hello exist in this class
+        print(f"I am a teacher, my name is {self.name}, I am {self.age} years old, I teach in {self.program} my email address is {self.email} and I work at {self.workplace}\n")
+        # return Student.say_hello(self)
 
 
-student2 = Student("Petra", 25, "petra@student.se", "")
-print("Student name:",student2.name)
+student1 = Student("Maggan", 23, "maggan@student.se", "Data Science")
+student2 = Student("Ove", 25, "ove@student.se", "")
+teacher1 = Teacher("Gudrun", 51, "gudrun@teacher.com", "ITHS", "Data Science")
+teacher2 = Teacher("Petra", 30, "petra@teacher.com", "GU", "Data Science")
+
+
+people = [student1, student2, teacher1, teacher2]
+
+# polymorphic
+for person in people:
+    person.say_hello()
+for person in people[:2]:
+    person.study()
+for person in people[2:]:
+    person.teach()
+
