c580f96011828f152cac296deaf3e3e410864042
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-10-01 14:25:46 +0200

    upd and move random_coord

---
+++ Labs_and_Explorations/Lab3/random_coord.py
@@ -0,0 +1,13 @@
+# Create random x, y, z
+# limits -10, 10 is just for testing script
+
+import random as rnd
+
+
+def random_coordinates():
+    point_x = rnd.randint(-10, 10)
+    point_y = rnd.randint(-10, 10)
+    point_z = rnd.randint(-10, 10)
+
+    return point_x, point_y, point_z
+    
